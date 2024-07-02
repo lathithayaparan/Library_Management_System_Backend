b193ebab2750ccb102c4c720fcf921f1b79a6b4e
Paranietharan <paranietharan64@gmail.com>
2024-07-02 23:18:49 +0530

    Complaints works done

--- src/main/java/com/alphacodes/librarymanagementsystem/DTO/ComplaintViewDto.java
+++ src/main/java/com/alphacodes/librarymanagementsystem/DTO/ComplaintViewDto.java
@@ -3,10 +3,12 @@ package com.alphacodes.librarymanagementsystem.DTO;
 import lombok.Data;
 
 @Data
-public class ComplaintDto {
+public class ComplaintViewDto {
+    private Long complaintId;
     private String userID;
     private String complaintDescription;;
     private String complaintType;
     private String complaintDate;
     private String complaintTime;
+    private boolean isResolved;
 }
